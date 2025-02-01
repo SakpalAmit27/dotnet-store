010060e8847e492e26ae5c639cc48e7d6703c019
Sakpal Amit <amitsakpal744@gmail.com>
2025-02-01 16:53:54 +0530

    inital commit , post build

--- store/obj/Debug/net9.0/store.AssemblyInfo.cs
+++ store/obj/Debug/net9.0/store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6edd7ea468078967436cd8aea1325c62916c6fcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
