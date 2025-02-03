61cd1b6d0b65590cba7dbd68089e3a32dfbf9490
Sakpal Amit <amitsakpal744@gmail.com>
2025-02-03 12:56:38 +0530

    filled out the Dtos , that we implement in list format  , and chaned the schema model , streams to decimal

--- store/Dtos/StoreDto.cs
+++ store/Dtos/StoreDto.cs
@@ -3,7 +3,7 @@ namespace store.Dtos;
 public record class StoreDto(int Id,
   string AlbumName, 
   string Genre,
-  string? Streams, 
+  decimal Streams, 
   string Artist,
   DateOnly ReleaseDate){
 
--- store/Program.cs
+++ store/Program.cs
@@ -1,7 +1,18 @@
+using store.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+List<StoreDto> store = [
+
+     new StoreDto(1, "Master of Puppets", "Thrash Metal",7.5M, "Metallica", new DateOnly(1986, 7, 1)),
+    new StoreDto(7, "Led Zeppelin IV", "Hard Rock", 37.2M, "Led Zeppelin", new DateOnly(1971, 11, 8)),
+    new StoreDto(8, "Slayer", "Thrash Metal",5.5M, "Reign in Blood", new DateOnly(1992, 8, 12)),
+    new StoreDto(9, "Ride the Lightning", "Thrash Metal",4.2M, "Metallica", new DateOnly(1984, 11, 21)),
+    new StoreDto(10, "Purpose", "Pop", 26.2M, "Justin Beiber", new DateOnly(2015, 9, 21))
+
+];
+
 app.Run();
- 
